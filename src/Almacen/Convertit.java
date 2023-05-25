668afe3882aab8ea4a43377cc6e0913ff5f52c81
52551 <52551@LAPTOP-CT8EMV28>
2023-05-25 15:24:17 -0600

    cesar alexis actualizacion de tranajp y lectura de archivos

---
+++ src/Almacen/Convertit.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Almacen;
+
+import Informacion.Convertir;
+
+/**
+ *
+ * @author 52551
+ */
+public class Convertit extends Convertir {
+
+    public Convertit() {
+    }
+    
+}
---
+++ src/g4s21facap10/G4s21facaP10.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package g4s21facap10;
+
+/**
+ *
+ * @author 52551
+ */
+public class G4s21facaP10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new JFPrincipal().setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
